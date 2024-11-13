962410801559dcac36b8b3389c4eb9530f9ee40b
PPSXD <raynerchung888@gmail.com>
2024-11-13 17:01:07 +0800

    first commit

--- js/page.js
+++ js/page.js
@@ -3,6 +3,10 @@ function Avenue_of_Stars_previous_page(){
     window.open(`HongKong.html` , `_self`)
 }
 
+function TAIPEI_101_previous_page(){
+    window.open(`TaiBei.html` , `_self`)
+}
+
 function home(){
     window.open(`../index.html` , `_self`)
 }
