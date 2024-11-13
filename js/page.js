74af7a3f14abbcfde15b4cf90128fe7d1dc5763b
PPSXD <raynerchung888@gmail.com>
2024-11-13 17:45:36 +0800

    first commit

--- js/page.js
+++ js/page.js
@@ -7,6 +7,10 @@ function TAIPEI_101_previous_page(){
     window.open(`TaiBei.html` , `_self`)
 }
 
+function N_Seoul_Tower_previous_page(){
+    window.open(`Seoul.html` , `_self`)
+}
+
 function home(){
     window.open(`../index.html` , `_self`)
 }
